8a3de3fedbb88829f983eaf0a099c07f16e71aad
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-06-23 20:41:52 -0700

    worked on the offer page

--- frontend/src/pagesections/home-sections/weeklyOfferSection/OfferSection.tsx
+++ frontend/src/pagesections/home-sections/weeklyOfferSection/OfferSection.tsx
@@ -3,21 +3,47 @@ import "./offerSection.scss"
 import dividerImg from "../../../assets/images/divider.png"
 import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import "swiper/scss"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { useState } from "react";
 const OfferSection = () => {
-  return (
+    const [swiper, setSwiper] = useState<any>(null)
+
+    const SwipeLeft = () =>{
+        swiper.slidePrev()
+    }
+    const SwipeRight = () =>{
+        swiper.slideNext()
+    }
+    return (
     <section className="offerSection">
         <div className="offerSection-wrapper">
             <header className="offer-header">
                 <h1 className="offer-title">Weekly Pizza Offer</h1>
                 <img src={dividerImg} alt="" className="divider"/>
             </header>
-            <ArrowBackIosOutlinedIcon className="arrow-icon left"/>
-            <ArrowForwardIosOutlinedIcon className="arrow-icon right"/>
-            <div className="slider-card-container">                
-                <OfferCard/>
-                <OfferCard/>
-                <OfferCard/>
-                <OfferCard/>
+            <ArrowBackIosOutlinedIcon className="arrow-icon left" onClick={SwipeLeft}/>
+            <ArrowForwardIosOutlinedIcon className="arrow-icon right" onClick={SwipeRight}/>
+            <div className="slider-card-container">        
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={10} 
+                    onInit={(ev:any)=>{
+                        setSwiper(ev)
+                    }} 
+                    init={false}               
+                    className="mySwiper"
+                >       
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide>
+                    <SwiperSlide><OfferCard/></SwiperSlide> 
+                </Swiper>
             </div>
         </div>
     </section>
