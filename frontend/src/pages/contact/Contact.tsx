dc4fa594948b51275ac75eb9cb941def05b0b3b7
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-06-30 22:11:24 -0700

    worked on the contact page

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -1,6 +1,9 @@
 import { MessageOutlined } from "@mui/icons-material"
 import formImg from "../../assets/images/contactImg.svg"
 import "./contact.scss"
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 const Contact = () => {
   return (
     <section className="contact">
@@ -39,6 +42,32 @@ const Contact = () => {
                     </form>
                 </div>
                 <img src={formImg} alt="undraw-img for form" className="form-image" />
+            </div>
+            <div className="contact-info-cards">
+                <div className="info-card">
+                    <h3>Phone Number</h3>
+                    <PhoneInTalkIcon className="info-card-icon"/>
+                    <div className="contact-info">
+                        <p>+233 54 504 6757</p>
+                        <p>+233 20 821 3970</p>
+                    </div>
+                </div>
+                <div className="info-card">
+                    <h3>Email Address</h3>
+                    <MailOutlineIcon className="info-card-icon"/>
+                    <div className="contact-info">
+                        <p>afriyiesamuel36@gmail.com</p>
+                        <p>pizzaorder@gmail.com</p>
+                    </div>
+                </div>
+                <div className="info-card">
+                    <h3>our location</h3>
+                    <LocationOnOutlinedIcon className="info-card-icon"/>
+                    <div className="contact-info">
+                        <p>12 Park Street Road</p>
+                        <p>New York, 15888, USA</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
