5ebb5b1978feb36bc48088d575d4f70554a3b926
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-07-03 18:55:14 -0700

    working on the restaurant details page

--- frontend/src/components/restaurantTabbar/RestaurantTabBar.tsx
+++ frontend/src/components/restaurantTabbar/RestaurantTabBar.tsx
@@ -1,18 +1,20 @@
 import "./restaurantTabBar.scss"
+import { useState } from "react"
 const RestaurantTabBar = () => {
+    const [selected, setSelected] = useState<string>("Pizza")
   return (
     <div className="restaurantTabBar">
         <div className="restaurantTabBar-wrapper">
-            <div className="rest-tab active">
+            <div className={selected === "Pizza" ? "rest-tab active" : "rest-tab"} onClick={()=>setSelected("Pizza")}>
                 <span>Pizza</span>
             </div>
-            <div className="rest-tab">
+            <div className={selected === "Soup" ? "rest-tab active" : "rest-tab"} onClick={()=>setSelected("Soup")}>
                 <span>Soup</span>
             </div>
-            <div className="rest-tab">
+            <div className={selected === "Breakfast" ? "rest-tab active" : "rest-tab"} onClick={()=>setSelected("Breakfast")}>
                 <span>Breakfast</span>
             </div>
-            <div className="rest-tab">
+            <div className={selected === "Lunch" ? "rest-tab active" : "rest-tab"} onClick={()=>setSelected("Lunch")}>
                 <span>Lunch</span>
             </div>
         </div>
