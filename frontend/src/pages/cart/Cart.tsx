6c8e13bd1f5fe5c7ef1dbae87c398dc38102e192
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-07-05 17:42:04 -0700

    worked on the cart page

--- frontend/src/pages/cart/Cart.tsx
+++ frontend/src/pages/cart/Cart.tsx
@@ -1,5 +1,9 @@
 import "./cart.scss"
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import { Add, Close, Remove } from '@mui/icons-material'
+import productImg from "../../assets/images/pizzaImg.png"
+import productImg1 from "../../assets/images/spizzza.png"
+
 const Cart = () => {
   return (
     <section className="cart">
@@ -13,7 +17,90 @@ const Cart = () => {
             </header>
             <div className="cart-body">
                 <div className="cart-body-left">
-
+                    <table>
+                        <thead>
+                            <tr className="table-head">
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div className="product-info">
+                                        <img src={productImg} alt="product-img" className="product-img"/>
+                                        <span className="product-title">Meat Pizza</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span className="product-price">$100.00</span>
+                                </td>
+                                <td>
+                                    <div className="amount-container">
+                                        <Add className="add"/>
+                                        <span>2</span>
+                                        <Remove className="remove"/>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="subtotal">
+                                        <span>$200.00</span>
+                                        <Close className="delete-icon"/>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div className="product-info">
+                                        <img src={productImg} alt="product-img" className="product-img"/>
+                                        <span className="product-title">Meat Pizza</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span className="product-price">$100.00</span>
+                                </td>
+                                <td>
+                                    <div className="amount-container">
+                                        <Add className="add"/>
+                                        <span>2</span>
+                                        <Remove className="remove"/>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="subtotal">
+                                        <span>$200.00</span>
+                                        <Close className="delete-icon"/>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div className="product-info">
+                                        <img src={productImg} alt="product-img" className="product-img"/>
+                                        <span className="product-title">Meat Pizza</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span className="product-price">$100.00</span>
+                                </td>
+                                <td>
+                                    <div className="amount-container">
+                                        <Add className="add"/>
+                                        <span>2</span>
+                                        <Remove className="remove"/>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="subtotal">
+                                        <span>$200.00</span>
+                                        <Close className="delete-icon"/>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div className="cart-body-right">
                     <div className="cart-totals">
