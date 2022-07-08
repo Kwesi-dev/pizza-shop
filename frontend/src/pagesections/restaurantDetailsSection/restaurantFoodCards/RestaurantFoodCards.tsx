0130710b1db4f3aa0d30637292092ed02a4371ce
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-07-07 20:10:29 -0700

    worked on the pagination

--- frontend/src/pagesections/restaurantDetailsSection/restaurantFoodCards/RestaurantFoodCards.tsx
+++ frontend/src/pagesections/restaurantDetailsSection/restaurantFoodCards/RestaurantFoodCards.tsx
@@ -4,11 +4,121 @@ import "./restaurantFoodCards.scss"
 import ReactPaginate from 'react-paginate';
 import { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+type cardstype = {
+    id : number
+}
+const card: cardstype[]= [
+    {
+        id: 1,
+    },
+    {
+        id: 2,
+    },
+    {
+        id: 3,
+    },
+    {
+        id: 4,
+    },
+    {
+        id: 5,
+    },
+    {
+        id: 6,
+    },
+    {
+        id: 7,
+    },
+    {
+        id: 8,
+    },
+    {
+        id: 9,
+    },
+    {
+        id: 10,
+    },
+    {
+        id: 11,
+    },
+    {
+        id: 12,
+    },
+    {
+        id: 13,
+    },
+    {
+        id: 14,
+    },
+    {
+        id: 15,
+    },
+    {
+        id: 16,
+    },
+    {
+        id: 17,
+    },
+    {
+        id: 18,
+    },
+    {
+        id: 19,
+    },
+    {
+        id: 20,
+    },
+    {
+        id: 21,
+    },
+    {
+        id: 22,
+    },
+    {
+        id: 23,
+    },
+    {
+        id: 24,
+    },
+    {
+        id: 25,
+    },
+    {
+        id: 26,
+    },
+    {
+        id: 27,
+    },
+]
+
 const RestaurantFoodCards = () => {
+  const itemsPerPage = 12
+  const [currentItems, setCurrentItems] = useState<cardstype[]>([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(card.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(card.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
+
+  const handlePageClick = (event: { selected: number}) => {
+    const newOffset = (event.selected * itemsPerPage) % card.length;
+    setItemOffset(newOffset);
+  };
+
   return (
     <section className="restaurantFoodCards">
         <div className="restaurantFoodCards-wrapper">
-            <div className="first-grid">
+            <div className="second-grid">
+                {
+                    currentItems.map((c)=>
+                        <OfferCard/>
+                    )
+                }
+            </div>
+            {/* <div className="first-grid">
                 <FoodCardH/>
                 <FoodCardH/>
             </div>
@@ -27,13 +137,14 @@ const RestaurantFoodCards = () => {
                 <OfferCard/>
                 <OfferCard/>
                 <OfferCard/>
-            </div>
+            </div> */}
             <ReactPaginate
+                onPageChange={handlePageClick}
                 breakLabel="..."
                 nextLabel={<ChevronRight className="next-button"/>}
                 pageRangeDisplayed={3}
                 previousLabel={<ChevronLeft className="prev-button"/>}
-                pageCount={12}
+                pageCount={pageCount}
                 containerClassName="pagination-container"
                 previousClassName="previous-li"
                 nextClassName="next-li"
