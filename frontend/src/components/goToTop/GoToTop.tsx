bc7a8ad88e79abd980dafaafd637ca063d854e07
Afriyie Samuel <afriyiesamuel36@gmail.com>
2022-07-15 14:21:35 -0700

    working on the responsiveness

--- frontend/src/components/goToTop/GoToTop.tsx
+++ frontend/src/components/goToTop/GoToTop.tsx
@@ -2,11 +2,9 @@ import { useLocation } from "react-router-dom"
 import { useEffect } from "react"
 const GoToTop = () => {
   const routePath = useLocation()
-  console.log(routePath)
   const toTop = () => {
     window.scrollTo(0, 0)
   }
-
   useEffect(()=>{
     toTop()
   }, [routePath])
--- frontend/src/components/restaurantTabbarMobile/RestaurantTabbarMobile.tsx
+++ frontend/src/components/restaurantTabbarMobile/RestaurantTabbarMobile.tsx
@@ -1,19 +1,23 @@
 import { Menu } from '@mui/icons-material'
 import "./restaurantTabbarMobile.scss"
+import { useState } from 'react'
 const RestaurantTabbarMobile = () => {
+  const [selectedTab, setSelectedTab] = useState("Pizza")
+  const [menuActive, setMenuActive] = useState(false)
   return (
     <article className="restTabbarMobile">
         <div className="restTabbarMobile-topbar">
             <div className="selected-tab">
-                <p>Pizza</p>
+                <p>{selectedTab}</p>
             </div>
-            <Menu className="menu-icon"/>
+            <Menu className="menu-icon" onClick={()=> setMenuActive(!menuActive)}/>
         </div>
-        <div className="slide-menu">
+        <div className={menuActive === true ? "slide-menu menu-active" : "slide-menu"}>
             <ul>
-                <li>Soup</li>
-                <li>Breakfast</li>
-                <li>Lunch</li>
+                <li onClick={() => setSelectedTab("Pizza")}>Pizza</li>
+                <li onClick={() => setSelectedTab("Soup")}>Soup</li>
+                <li onClick={() => setSelectedTab("Breakfast")}>Breakfast</li>
+                <li onClick={() => setSelectedTab("Lunch")}>Lunch</li>
             </ul>
         </div>
     </article>
