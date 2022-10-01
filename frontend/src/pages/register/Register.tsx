a5fd047b1993cf22f084fbca0220eac30ed0b420
Afriyie Samuel Kwesi <afriyiesamuel36@gmail.com>
2022-09-30 23:51:50 -0700

    feat: addd react hook form

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -3,22 +3,48 @@ import LockIcon from '@mui/icons-material/Lock';
 import PersonIcon from '@mui/icons-material/Person';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import facebooklogo from "../../assets/images/facebook.png"
 import googlelogo from "../../assets/images/google.png"
 import twitterlogo from "../../assets/images/twitter.png"
 import backgroundImg from "../../assets/images/background.png"
 import "./register.scss"
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form"
+import { axiosInstance } from "../../http/axiosRequests";
 
+type formInputs = {
+  email: string,
+  password: string,
+}
 type registerProps =  {
     setShowNav:  React.Dispatch<React.SetStateAction<boolean>>
   }
 const Register = ({ setShowNav }: registerProps) => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
+  const { register, handleSubmit, reset, formState: { errors, isSubmitSuccessful, dirtyFields } } = useForm<formInputs>({
+    defaultValues:{
+      email: "",
+      password: "",
+    }
+  })
   const [passwordVisible, setPasswordVisible] = useState<boolean>(false)
+  
+  useEffect(() => {
+    if(isSubmitSuccessful){
+      reset()
+    }
+  }, [isSubmitSuccessful, reset])
+
+  const onSubmit: SubmitHandler<formInputs> = async (data) => {
+    try{
+      const res = await axiosInstance.post("/auth/register", data)
+      console.log("res", res);
+    }catch(err){
+      console.log(err)
+    }  
+
+  }
+  
   return (
     <div className="register">
         <img src={backgroundImg} alt="" className="background" />
@@ -42,28 +68,47 @@ const Register = ({ setShowNav }: registerProps) => {
                 <h1>welcome customer!</h1>
                 <p>Register to continue</p>
               </div>
-                <form>
-                    <div className="input-group">
-                      <PersonIcon className="input-icon"/>
-                      <input type="text" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="username!!@gmail.com"/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="input-group-container">
+                      <div className={errors.email ? "input-group input-error" : "input-group"}>
+                        <PersonIcon className="input-icon"/>
+                        <input type="email" placeholder="username!!@gmail.com" {...register("email", {
+                          required : "email is required"
+                        })}/>
+                      </div>
+                      <p className="error">{errors.email?.message}</p>
                     </div>
-                    <div className="input-group">
-                      <LockIcon className="input-icon"/>
-                      <input type={passwordVisible === true ? "text" : "password"} name="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password"/>
-                      {password.length > 0 &&
-                        <>
-                        {passwordVisible === true ? 
-                          <VisibilityOffIcon className="visible-icon" onClick={() => setPasswordVisible(!passwordVisible)}/>
-                          :
-                          <VisibilityIcon className="visible-icon" onClick={() => setPasswordVisible(!passwordVisible)}/>
+                    <div className="input-group-container">
+                      <div className={errors.password ? "input-group input-error" : "input-group"}>
+                        <LockIcon className="input-icon"/>
+                        <input type={passwordVisible === true ? "text" : "password"} placeholder="Enter Password" {...register("password", {
+                          minLength:{
+                            value: 6,
+                            message: "password must have atleast 6 characters"
+                          },
+                          required: "password must have atleast 6 characters"
+                        })}/>
+                        {dirtyFields.password &&
+                          <>
+                          {passwordVisible === true ? 
+                            <VisibilityOffIcon className="visible-icon" onClick={() => setPasswordVisible(!passwordVisible)}/>
+                            :
+                            <VisibilityIcon className="visible-icon" onClick={() => setPasswordVisible(!passwordVisible)}/>
+                          }
+                          </>
                         }
-                        </>
-                      }
+                      </div>
+                      <p className="error">{errors.password?.message}</p>
                     </div>
-                    <div className="input-group">
+                    {/* <div className="input-group">
                       <LockIcon className="input-icon"/>
-                      <input type="password" name="confirm" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} placeholder="Confirm Password"/>
-                    </div>
+                      <input type="password" placeholder="Confirm Password" { ...register("confirmPassword", {
+                        required: true,
+                        pattern:{
+                          value: "cjajskjsaj"
+                        }
+                      })}/>
+                    </div> */}
                     <div className="form-buttons">
                       <button className="register-btn">Register</button>
                     </div>
